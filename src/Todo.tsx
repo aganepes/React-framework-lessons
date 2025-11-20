61b4a284ebc61ad973c0cbcf14758dfccb77c827
Aganepes <aaganepes@gmail.com>
2025-11-20 19:01:08 +0500

    remove useEffect hook in useRef() lesson

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import type { todoType } from './App';
 import styles from "./todo.module.css";
 
