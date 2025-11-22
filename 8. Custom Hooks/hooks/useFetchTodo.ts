5fc7640ee80057ceab7d8a5a374cd09d58a6f092
Aganepes <aaganepes@gmail.com>
2025-11-22 16:53:51 +0500

    add to custom hooks lesson

---
+++ 8. Custom Hooks/hooks/useFetchTodo.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import type { todoType } from "../App";
+
+type fetchTodo = { data: todoType[], error: unknown, loading: boolean };
+
+const useFetchTodo = (userId: number): fetchTodo => {
+	const [data, setData] = useState<todoType[]>([]);
+	const [error, setError] = useState<unknown>(null);
+	const [loading, setLoading] = useState<boolean>(true);
+
+	const fetchTodo = async () => {
+		try {
+			const response = await fetch("http://localhost:3000/todos");
+			const data = (await response.json()) as todoType[];
+			setData(data.filter(t => t.userId === userId));
+		} catch (error) {
+			setError(error);
+		} finally {
+			setLoading(false);
+		}
+	}
+	useEffect(() => {
+		fetchTodo();
+	}, [userId]);
+
+	return { data, error, loading };
+}
+
+export default useFetchTodo;
