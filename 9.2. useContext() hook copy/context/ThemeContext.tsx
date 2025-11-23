f34448e855b0ceca50ff1c5a29a7bd4261ddb60b
Aganepes <aaganepes@gmail.com>
2025-11-23 22:43:27 +0500

    change to folder name of useContext() lessons

---
+++ 9.2. useContext() hook copy/context/ThemeContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, useReducer, type FC, type ReactNode } from "react";
+
+
+interface ThemeState {theme: "light" | "dark"}
+type ThemeAction = { type: "TOGGLE_THEME" };
+
+export type ThemeContextProps = {
+	state: ThemeState;
+	dispatch: React.Dispatch<ThemeAction>;
+} | null;
+
+const themeReducer = (state: ThemeState, action: ThemeAction): ThemeState => {
+	switch (action.type) {
+		case "TOGGLE_THEME":
+			return {
+				...state,
+				theme: state.theme == "dark" ? "light" : "dark",
+			}
+		default:
+			return state
+	}
+}
+
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const ThemeContext = createContext<ThemeContextProps>(null);
+
+interface ThemeProviderProps {
+	children: ReactNode
+}
+
+export const ThemeProvider: FC<ThemeProviderProps> = ({ children }) => {
+	const [state, dispatch] = useReducer(themeReducer, { theme: "light" });
+	const contextValue: ThemeContextProps = { state, dispatch };
+
+	return (
+		<ThemeContext.Provider value={contextValue}>
+			{children}
+		</ThemeContext.Provider>
+	)
+}
